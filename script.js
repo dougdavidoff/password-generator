91d18f5ddf15e501340db8e748a58ee9a83a2d1c
Doug Davidoff <douglass.davidoff@gmail.com>
2020-08-20 22:06:27 -0400

    MAJOR REVISE to password generator files

--- script.js
+++ script.js
@@ -1,63 +1,62 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Password variable holds the generated password
+preGen = " "
+securePW = "";
 
 
+// Charsets gives the character set to choose from
+charLower = "abcdefghijklmnopqrstuvwxyz";
+charUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+charNum = "1234567890";
+charSpec = "!@#$%^&*";
+var i = 0;
 
+// Event listener begins the sequence
+document.getElementById("generatePass").addEventListener("click", passGenerate);
 
+// Function to generate password
+function passGenerate() {
+    // Input creates the length variable and logs it
+    var passLength = prompt("How long should your password be? Enter a number between 8 and 128 to choose the number of characters your password should contain:");
+    console.log("User wants the password to be " + passLength + " characters long");
+    // Asks user what characters they want to use
+    var input = confirm("Would you like to use lower-case letters? Click 'OK' for 'yes;' 'Cancel' for 'no.'");
+    if (input == true) {
+        preGen += charLower;
+        console.log("User wants to use lower-case letters. The characters to be used so far are: " + preGen);
+    }
+    var input = confirm("Would you like to use upper-case letters? Click 'OK' for 'yes;' 'Cancel' for 'no.'");
+    if (input == true) {
+        preGen += charUpper;
+        console.log("User wants to use upper-case letters. The characters to be used so far are: " + preGen);
+    }
+    var input = confirm("Would you like to use numbers? Click 'OK' for 'yes;' 'Cancel' for 'no.'")
+    if (input == true) {
+        preGen += charNum;
+        console.log("User wants to use numbers. The characters to be used so far are: " + preGen);
+    }
+    var input = confirm("Would you like to use special symbols? Click 'OK' for 'yes;' 'Cancel' for 'no.'");
+    if (input == true) {
+        preGen += charSpec;
+        console.log("User wants to use special symbols. The characters to be used are: " + preGen);
+    }
 
-<script>
-
-// on.click here
-
-var product = "sample";
-var lowers = "abcdefghijklmnopqrstuvwxyz";
-var uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var digits = "0123456789";
-var specials = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~"
-
-var length = prompt("How many characters would you like in your password? Choose a number between 8 and 128.");
-
-var lowerCase = confirm("Do you want lower-case letters in your password?");
-
-var upperCase = confirm("Do you want upper-case letters in your password?");
-
-var numerics = confirm("Do you want numbers in your password?");
-
-var specialCharacters = confirm("Do you want special characters in your password?");
-
-// var types = [lowCase, upCase, num, spec]
-
-// var types=[lowerCase, upperCase, numerics, specialCharacters];
-
-// for i = 0; i < length; i++;
-
-// pickTry = Math.floor((Math.random() * 4));
-
-// if pickTry = true; randomly select pickCharacter from PickTry's array
+    // Asks user if what they chose is correct and makes sure a character type was chosen
     
-// else, try again
 
-// attach pickCharacter to product
+    // Password is generated here meeting the criteria
 
-// try again
+     
+    console.log("passLength is " + passLength);
+    console.log("preGen is " + preGen);
 
-// end for loop
+            for (var i = 0; i < passLength; i++)  { 
+            securePW +=  preGen[Math.floor(Math.random() * preGen.length)];
+            console.log(securePW); 
+        } 
+    
+    console.log("The finished password is " + securePW);
 
-// display product
-document.getElementById("product").innerHTML = "Your password is " + product;
-
-Alert("Thank you for choosing us today!");
-
-</script>
+    var password = securePW;
+    var passwordText = document.querySelector("#password");
+    passwordText.value = securePW;
+          }
---
+++ script.old.js
@@ -0,0 +1,138 @@
+document.getElementById("user-button").addEventListener("click", displayDate);
+
+function displayDate() {
+  document.getElementById("demo").innerHTML = Date();
+}
+
+
+
+
+
+
+
+
+
+
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//     var password = generatePassword();
+//     var passwordText = document.querySelector("#password");
+  
+//     passwordText.value = password;
+  
+//   }
+  
+//   // Add event listener to generate button
+//   generateBtn.addEventListener("click", writePassword);
+  
+
+
+
+
+
+
+
+
+
+// // var generateBtn = document.querySelector("#generate");
+
+// // // document.getElemengtById("myBtn").addEventListener("click");
+
+// // // var product = "AbCd1234!*&%";
+
+// // // var display = "Hello";
+// // // document.getElementById("mytext").value = test;
+
+// // // $(document).ready(function(){
+// // //     $("#searchimg").click(function(){
+// // //       $("#search").slideToggle();
+// // //     });
+// // //   });
+// // //    function content(){
+// // //       var div = document.getElementById("textDiv");
+// // //       div.textContent = "my text";
+// // //       var text = div.textContent;
+// // //       var div = document.getElementById("textDiv");
+// // //       div.style="font-size:24px";
+// // //    }
+
+// // // document.getElementById("myBtn").addEventListener("click", displayDate);
+
+// // function displayDate() {
+// //   document.getElementById("demo").innerHTML = Date();
+// // }
+
+// var test = 4 + 5 + "!*&Hello";
+// document.getElementById("password").value = test;
+
+
+
+
+
+
+
+
+
+
+// // // // // // Write password to the #password input
+// // // // // function writePassword() {
+// // // // //   var password = generatePassword();
+// // // //   var passwordText = document.querySelector("#password");
+
+// // // //   passwordText.value = password;
+
+// // // // }
+
+// // // // Add event listener to generate button
+// // // generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+// // // <script>
+
+// // // // on.click here
+
+// // // var product = "sample";
+// // // var lowers = "abcdefghijklmnopqrstuvwxyz";
+// // // var uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// // // var digits = "0123456789";
+// // // var specials = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~"
+
+// // // var length = prompt("How many characters would you like in your password? Choose a number between 8 and 128.");
+
+// // // var lowerCase = confirm("Do you want lower-case letters in your password?");
+
+// // // var upperCase = confirm("Do you want upper-case letters in your password?");
+
+// // // var numerics = confirm("Do you want numbers in your password?");
+
+// // // var specialCharacters = confirm("Do you want special characters in your password?");
+
+// // // // var types = [lowCase, upCase, num, spec]
+
+// // // // var types=[lowerCase, upperCase, numerics, specialCharacters];
+
+// // // // for i = 0; i < length; i++;
+
+// // // // pickTry = Math.floor((Math.random() * 4));
+
+// // // // if pickTry = true; randomly select pickCharacter from PickTry's array
+    
+// // // // else, try again
+
+// // // // attach pickCharacter to product
+
+// // // // try again
+
+// // // // end for loop
+
+// // // // display product
+// // // document.getElementById("product").innerHTML = "Your password is " + product;
+
+// // // Alert("Thank you for choosing us today!");
+
+// // // </script>
